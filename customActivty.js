a2c198578194b79c3dd90aa9ab83d8abf7b5ee6d
nefrance <nerea0980@gmail.com>
2024-04-11 15:24:24 +0200

    repo

---
+++ customActivty.js
@@ -0,0 +1,16 @@
+var connection  = new Postmonger.Session();
+
+$(window).ready(onRender); // cuando esté lista la ventana del iframe llama a la función onRender
+
+function onRender() {
+    connection.trigger('ready'); // avisa a Postmonger de que la app ya está lista
+}
+
+connection.on('initActivity', function(data) { // cuando se haga el trigger de ready, se ejecuta esta función
+    if(data) { payload = data } // valida si tenemos alguna configuración (en este caso sí, en el fichero config.json)
+}); 
+
+connection.on('clickedNext', function() {
+    payload['metaData'].isConfigured = true;
+    connection.trigger('updateActivity', payload);
+});
